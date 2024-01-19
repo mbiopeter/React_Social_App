ead6c8b580cac8e0137aba5831d7eb200e4d4453
mbiopeter <mbiopeter401@gmail.com>
2024-01-19 20:14:17 +0300

    Protecting the Routes

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { createBrowserRouter, Outlet, RouterProvider } from "react-router-dom";
+import { createBrowserRouter, Navigate, Outlet, RouterProvider } from "react-router-dom";
 import Login from '../src/pages/login/Login.jsx';
 import Home from '../src/pages/home/Home.jsx';
 import Profile from '../src/pages/profile/Profile.jsx';
@@ -7,6 +7,10 @@ import NavBar from "./components/navBar/NavBar.jsx";
 import LeftBar from "./components/leftBar/LeftBar.jsx";
 import RightBar from "./components/rightBar/RightBar.jsx";
 function App() {
+
+  const currentUser = true;
+
+
   const Layout = () => {
     return (
       <div>
@@ -21,10 +25,20 @@ function App() {
     )
   }
 
+  const ProtectedRoute = ({ children }) => {
+    if (!currentUser) {
+      return <Navigate to="/Login" />
+    }
+    return children;
+  }
+
   const router = createBrowserRouter([
     {
       path: "/",
-      element: <Layout />,
+      element: (
+        <ProtectedRoute>
+          <Layout />
+        </ProtectedRoute>),
       children: [
         {
           path: "/",
