8ca051f5a02540b11566aecce51a3da01d07c44a
mbiopeter <mbiopeter401@gmail.com>
2024-01-20 20:53:08 +0300

    stories

---
+++ src/components/posts/Posts.jsx
@@ -0,0 +1,11 @@
+import "./Posts.scss";
+
+const Posts = () => {
+  return (
+    <div className='posts'>
+      Posts
+    </div>
+  )
+}
+
+export default Posts
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,9 +1,12 @@
+import Posts from "../../components/posts/Posts"
+import Stories from "../../components/stories/Stories"
 import "./Home.scss"
 
 const Home = () => {
   return (
     <div className="home">
- 
+      <Stories/>
+      <Posts />
     </div>
   )
 }
