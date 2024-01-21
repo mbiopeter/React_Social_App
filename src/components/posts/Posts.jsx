bb47725217665c70ab0b374e5342fbe079fd3bfc
mbiopeter <mbiopeter401@gmail.com>
2024-01-21 15:12:05 +0300

    posts

--- src/components/posts/Posts.jsx
+++ src/components/posts/Posts.jsx
@@ -1,9 +1,34 @@
 import "./Posts.scss";
+import Post from "../post/Post"
 
 const Posts = () => {
+
+  //TEMPORARY
+  const posts = [
+    {
+      id: 1,
+      name: "John Doe",
+      userId: 1,
+      profilePic:
+        "https://images.pexels.com/photos/1308881/pexels-photo-1308881.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+      desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit",
+      img: "https://images.pexels.com/photos/1308881/pexels-photo-1308881.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+    },
+    {
+      id: 2,
+      name: "Jane Doe",
+      userId: 2,
+      profilePic:
+        "https://images.pexels.com/photos/1308881/pexels-photo-1308881.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+      desc: "Tenetur iste voluptates dolorem rem commodi voluptate pariatur, voluptatum, laboriosam consequatur enim nostrum cumque! Maiores a nam non adipisci minima modi tempore.",
+    },
+  ];
+
   return (
     <div className='posts'>
-      Posts
+      {posts.map(post => (
+        <Post post={post} key={post.id}/>
+      ))}
     </div>
   )
 }
